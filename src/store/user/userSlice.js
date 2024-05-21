277480eea2300193b560a5b8522697eb39484eb1
vanthan04 <thanvantran1707@gmail.com>
2024-05-22 01:33:22 +0700

    fixx

---
+++ src/store/user/userSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    isLoggin: false,
+    token: null,
+    isLoading: false,
+    mes: "",
+  },
+
+  reducers: {
+    login: (state, action) => {
+        state.isLoggin = action.payload.isLoggin;
+        state.token = action.payload.token;
+        localStorage.setItem("token", action.payload.token);
+    },
+    logout: (state, action) => {
+        state.isLoggin = false;
+        state.token = null;
+        localStorage.clear();
+    },
+  },
+
+});
+
+export const { login, logout, clearMessage } = userSlice.actions;
+export default userSlice.reducer;
