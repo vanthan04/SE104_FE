2459f81192ca3456ee7b4967a2a112d6c6e16504
Tam Anh Nguyen Duy <nguyenduytamanh03012004@gmail.com>
2024-05-22 00:00:19 +0700

    update Login & router

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,13 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
-import { HomePage } from "../pages/Home";
 import ManageLibPage from "../pages/ManageLib";
 import { BookManagementPage } from "../pages/ManageLib/pages/BookManagement";
 import { ReaderManagementPage } from "../pages/ManageLib/pages/ReaderManagement";
+import LoginForm from "../components/Auth/Login";
 
 const router = createBrowserRouter([
      {
           path: '/',
-          element: <HomePage />
+          element: <LoginForm />
      },
      {
           path: '/librarian',
