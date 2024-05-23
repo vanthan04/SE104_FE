ba5ebb96d80915710c17ce1445a60781f49be5a4
vanthan04 <thanvantran1707@gmail.com>
2024-05-23 18:03:07 +0700

    update more files

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import adminReducer from "./admin/userSlice";
+import userReducer from "./user/userSlice";
 
 export const store = configureStore({
   reducer: {
-    admin: adminReducer
+    user: userReducer
   }
 });
--- src/store/user/userSlice.js
+++ src/store/user/userSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
-export const adminSlice = createSlice({
-  name: 'admin',
+export const userSlice = createSlice({
+  name: 'user',
   initialState: {
     isLoggin: false,
     token: null,
@@ -19,12 +19,10 @@ export const adminSlice = createSlice({
         state.token = null;
         localStorage.clear();
     },
-    clearMessage: (state) => {
-      state.mes = "";
-    },
+
   },
 
 });
 
-export const { login, logout, clearMessage } = adminSlice.actions;
-export default adminSlice.reducer;
+export const { login, logout } = userSlice.actions;
+export default userSlice.reducer;
